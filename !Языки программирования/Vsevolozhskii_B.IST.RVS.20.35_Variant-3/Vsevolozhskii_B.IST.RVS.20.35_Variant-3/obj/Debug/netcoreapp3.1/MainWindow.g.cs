6e5ca61564bf8048728576f9461be7767055efb3
SevaRizh <sevala2@yandex.ru>
2021-12-14 17:35:42 +0300

    TI start

--- !Языки программирования/Vsevolozhskii_B.IST.RVS.20.35_Variant-3/Vsevolozhskii_B.IST.RVS.20.35_Variant-3/obj/Debug/netcoreapp3.1/MainWindow.g.cs
+++ !Языки программирования/Vsevolozhskii_B.IST.RVS.20.35_Variant-3/Vsevolozhskii_B.IST.RVS.20.35_Variant-3/obj/Debug/netcoreapp3.1/MainWindow.g.cs
@@ -71,13 +71,13 @@ namespace Vsevolozhskii_B.IST.RVS._20._35_Variant_3 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Vsevolozhskii_B.IST.RVS.20.35_Variant-3;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Vsevolozhskii_B.IST.RVS.20.35_Variant-3;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -87,7 +87,7 @@ namespace Vsevolozhskii_B.IST.RVS._20._35_Variant_3 {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
